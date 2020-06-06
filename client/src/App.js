4a9f64f5c8b7e7a45479c592584d566b0cd1698b
joegturner <59180847+joegturner@users.noreply.github.com>
2020-06-06 01:02:27 -0400

    fixed meta data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Import Components
-import Header from './components/Header';
-import Courses from './components/Courses';
-import CourseDetail from './components/CourseDetail';
-import CreateCourse from './components/CreateCourse';
-import UpdateCourse from './components/UpdateCourse';
-import UserSignIn from './components/UserSignIn';
-import UserSignUp from './components/UserSignUp';
-import UserSignOut from './components/UserSignOut';
-import UnhandledError from './components/UnhandledError';
-import NotFound from './components/NotFound';
-import Forbidden from './components/Forbidden';
-import PrivateRoute from './PrivateRoute';
+import Header from "./components/Header";
+
+import Courses from "./components/Courses";
+import CourseDetail from "./components/CourseDetail";
+import CreateCourse from "./components/CreateCourse";
+import UpdateCourse from "./components/UpdateCourse";
+import UserSignIn from "./components/UserSignIn";
+import UserSignUp from "./components/UserSignUp";
+import UserSignOut from "./components/UserSignOut";
+import UnhandledError from "./components/UnhandledError";
+import NotFound from "./components/NotFound";
+import Forbidden from "./components/Forbidden";
+import PrivateRoute from "./PrivateRoute";
 
 // Consumer wrapper
-import withContext from './Context';
+import withContext from "./Context";
 const HeaderWithContext = withContext(Header);
 
 export default () => (
@@ -29,12 +26,22 @@ export default () => (
       <HeaderWithContext />
       <Switch>
         <Route exact path="/" component={withContext(Courses)} />
-        <Route path = '/signin' component={withContext(UserSignIn)} />
-        <Route path = '/signup' component={withContext(UserSignUp)} />
-        <Route path = '/signout' component={withContext(UserSignOut)} />
-        <PrivateRoute path="/courses/create" component={withContext(CreateCourse)} />
-        <PrivateRoute path="/courses/:id/update" component={withContext(UpdateCourse)} />
-        <Route exact path="/courses/:id" component={withContext(CourseDetail)} />
+        <Route path="/signin" component={withContext(UserSignIn)} />
+        <Route path="/signup" component={withContext(UserSignUp)} />
+        <Route path="/signout" component={withContext(UserSignOut)} />
+        <PrivateRoute
+          path="/courses/create"
+          component={withContext(CreateCourse)}
+        />
+        <PrivateRoute
+          path="/courses/:id/update"
+          component={withContext(UpdateCourse)}
+        />
+        <Route
+          exact
+          path="/courses/:id"
+          component={withContext(CourseDetail)}
+        />
         <Route path="/error" component={UnhandledError} />
         <Route path="/forbidden" component={Forbidden} />
         <Route path="/notfound" component={NotFound} />
